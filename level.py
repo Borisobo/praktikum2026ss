20afefb85ed7d8581e9d1eaad9db58bb1c47c56a
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-04 22:24:05 +0200

    Implement boundary checks for player in Level class
    Added boundary checking for player movement based on level dimensions.

--- level.py
+++ level.py
@@ -1,4 +1,23 @@
 import pygame 
+from settings import HEIGHT , WIDTH
 
 class Level :
-  def __init__(self)
+  def __init__(self) :
+    self.width = WIDTH
+    self.height = HEIGHT
+    
+  def check_boundaries(self, player):
+        if player.rect.left < 0:
+            player.rect.left = 0
+        # Правая граница
+        if player.rect.right > self.width:
+            player.rect.right = self.width
+        # Верхняя граница
+        if player.rect.top < 0:
+            player.rect.top = 0
+        # Нижняя граница
+        if player.rect.bottom > self.height:
+            player.rect.bottom = self.height
+        # Синхронизируем координаты
+        player.x = player.rect.x
+        player.y = player.rect.y
