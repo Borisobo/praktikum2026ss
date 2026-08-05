1324d13754190d8fe01cec35756bfe858fa0dbd2
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-05 17:12:42 +0200

    Reformat level.py for consistency

--- level.py
+++ level.py
@@ -9,15 +9,15 @@ class Level :
   def check_boundaries(self, player):
         if player.rect.left < 0:
             player.rect.left = 0
-        # Правая граница
+      
         if player.rect.right > self.width:
             player.rect.right = self.width
-        # Верхняя граница
+        
         if player.rect.top < 0:
             player.rect.top = 0
-        # Нижняя граница
+        
         if player.rect.bottom > self.height:
             player.rect.bottom = self.height
-        # Синхронизируем координаты
+          
         player.x = player.rect.x
         player.y = player.rect.y
