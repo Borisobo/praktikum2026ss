33549560e640fb8866f83307b64ccb0158417df5
grimmward13 <grimmward13@gmail.com>
2026-08-06 00:19:19 +0200

    Update cat.py

--- cat.py
+++ cat.py
@@ -1,7 +1,20 @@
 import pygame
 import random
+import os
 
-Class Cat:
 
-  def __init__(self)
-self.screen = 
+class Cat:
+
+  cats = []
+  for file in os.listdir("assets/cats.nps"):
+    image = pygame.image.load("assets/cats.nps" + file).covert_alpha()
+    cats.append(image)
+    cat = random.choice(cats)
+
+  def __init__(self, image, name, rating, qualities):
+    self._image = image
+    self._name = name
+    self._rating = rating
+    self._qualities = qualities
+    
+    
