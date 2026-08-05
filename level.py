c2ca2bd63f29cd74d8fa833d9790e25b9cd29184
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-05 21:54:25 +0200

    Update level.py

--- level.py
+++ level.py
@@ -1,11 +1,34 @@
 import pygame 
+#import random
 from settings import HEIGHT , WIDTH
+#from cat import Cat
+#from resource import Resource
+#from island import Island
 
 class Level :
   def __init__(self) :
     self.width = WIDTH
     self.height = HEIGHT
-    
+
+   # self.level_number = 1
+
+       # self.islands = []
+
+        #self.cats = []
+
+        #self.resources = []
+
+        #self.load_level()
+  #def load_level(self):
+    #if self.level_number == 1:
+        #self.load_level_1()
+  #def load_level_1(self):
+
+    #self.islands.clear()
+
+    #self.cats.clear()
+
+    #self.resources.clear(
   def check_boundaries(self, player):
         if player.rect.left < 0:
             player.rect.left = 0
