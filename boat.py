21ef62629074522cc1c9bd6daae779f59e6aa431
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-07 21:46:14 +0200

    Load and scale boat images in initialization
    Added image loading and scaling for boats.

--- boat.py
+++ boat.py
@@ -3,26 +3,31 @@ BOAT_LEVELS = {
         "max_cats": 2,
         "speed": 3.0,
         "upgrade_cost": 10
+        "image": "assets/boats/boat1.png"
     },
     2: {
         "max_cats": 4,
         "speed": 3.5,
         "upgrade_cost": 20
+        "image": "assets/boats/boat2.png"
     },
     3: {
         "max_cats": 6,
         "speed": 4.0,
         "upgrade_cost": 35
+        "image": "assets/boats/boat3.png"
     },
     4: {
         "max_cats": 8,
         "speed": 4.5,
         "upgrade_cost": 50
+        "image": "assets/boats/boat4.png"
     },
     5: {
         "max_cats": 10,
         "speed": 5.0,
         "upgrade_cost": None
+        "image": "assets/boats/boat5.png"
     }
 }
 
@@ -41,6 +46,12 @@ class Boat :
     self.speed = stats["speed"]
     self.upgrade_cost = stats["upgrade_cost"]
 
+    self.image = pygame.image.load(stats["image"]).convert_alpha()
+    self.image = pygame.transform.scale(self.image,(256, 200))
+    self.rect = self.image.get_rect(center=(self.x, self.y))
+
+  
+
   def upgrade(self):
     if self.level < 5:
         self.level += 1
@@ -58,4 +69,8 @@ class Boat :
   def remove_cat(self, cat):
     if cat in self.current_cats:
         self.current_cats.remove(cat)
+  def draw(self, screen):
+    screen.blit(self.image, self.rect
+
+  
     
