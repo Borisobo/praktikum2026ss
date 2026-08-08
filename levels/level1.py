ea59ab5d9b0e0921b5bb5177f6a6d3c4bc7f5372
Amir-Garifullin7 <garifullinamir.90@gmail.com>
2026-08-08 18:49:01 +0200

    Levels

---
+++ levels/level1.py
@@ -0,0 +1,25 @@
+
+spawn = (950, 1050)
+
+islands = [
+    {
+        "type": "library",
+        "image": "assets/islands/library.png",
+        "x": 250,
+        "y": 190,
+        "width": 350,
+        "height": 350,
+    },
+    {
+        "type": "island",
+        "image": "assets/islands/island2.png",
+        "x": 900,
+        "y": 500,
+        "width": 300,
+        "height": 300,
+    }
+]
+
+cats = [
+
+]
