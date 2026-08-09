96b9378baa36c4a3e46347d59d9f9d61fd97646a
Amir-Garifullin7 <garifullinamir.90@gmail.com>
2026-08-09 19:34:30 +0200

    Update player.py

--- player.py
+++ player.py
@@ -2,16 +2,17 @@ import pygame
 from settings import WIDTH, HEIGHT
 from boat import Boat
 
+
 class Player:
     def __init__(self):
-        self.image = pygame.image.load("assets/nyan_cat.png").convert_alpha()
+        self.image = pygame.image.load("assets/nyancat.png").convert_alpha()
         bounding = self.image.get_bounding_rect()
         self.image = self.image.subsurface(bounding).copy()
 
-        self.image = pygame.transform.scale(self.image, (32, 25))
+        self.image = pygame.transform.scale(self.image, (55, 40))
         self.mask = pygame.mask.from_surface(self.image)
 
-        self.rect = self.image.get_rect(midbottom=(WIDTH//2, HEIGHT - 130))
+        self.rect = self.image.get_rect(midbottom=(WIDTH // 2, HEIGHT - 130))
         self.x = float(self.rect.x)
         self.y = float(self.rect.y)
         self.vx = 0
@@ -20,9 +21,7 @@ class Player:
         self.max_speed = 3
         self.friction = 0.8
 
-        self.boat = Boat(WIDTH // 2,HEIGHT - 130)
-
-        
+        self.boat = Boat(WIDTH // 2, HEIGHT - 130)
 
     def draw(self, screen):
         self.boat.draw(screen)
@@ -37,9 +36,9 @@ class Player:
             self.vy -= self.acceleration
         if keys[pygame.K_DOWN]:
             self.vy += self.acceleration
-        #Begrenzung von max_speed
-        self.vx = max(-self.boat.speed,min(self.vx, self.boat.speed))
-        self.vy = max(-self.boat.speed,min(self.vy, self.boat.speed))
+        # Begrenzung von max_speed
+        self.vx = max(-self.boat.speed, min(self.vx, self.boat.speed))
+        self.vy = max(-self.boat.speed, min(self.vy, self.boat.speed))
         # Reibungskraft
         self.vx *= self.friction
         self.vy *= self.friction
@@ -47,10 +46,9 @@ class Player:
         self.y += self.vy
         self.rect.x = int(self.x)
         self.rect.y = int(self.y)
-        
+
         self.boat.x = self.x
         self.boat.y = self.y + 50
-        self.boat.rect.center = (int(self.boat.x),int(self.boat.y))
+        self.boat.rect.center = (int(self.boat.x), int(self.boat.y))
 
-
-
+        print(keys[pygame.K_UP], self.vy)
