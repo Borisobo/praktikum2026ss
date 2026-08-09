a94568c985f48f0d82b4545354c89633ab63b5f0
Amir-Garifullin7 <garifullinamir.90@gmail.com>
2026-08-10 00:28:43 +0200

    Update game.py

--- game.py
+++ game.py
@@ -1,8 +1,11 @@
 import pygame
 from settings import WIDTH, HEIGHT, FPS, TITLE
+from boat import Boat
 from player import Player
 from level import Level
 from ui import UI
+
+
 
 class Game:
     def __init__(self):
@@ -10,11 +13,10 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption(TITLE)
         self.clock = pygame.time.Clock()
-        self.player = Player()
-
-        self.level = Level()
         self.ui = UI(WIDTH, HEIGHT)
         self.selected_cat = None
+        self.player = Player()
+        self.level = Level()
 
         self.water_tiles = [
             pygame.transform.scale(
@@ -23,6 +25,14 @@ class Game:
             ),
             pygame.transform.scale(
                 pygame.image.load("assets/waterbig2.PNG").convert(),
+                (WIDTH, HEIGHT)
+            ),
+            pygame.transform.scale(
+                pygame.image.load("assets/waterbig3.PNG").convert(),
+                (WIDTH, HEIGHT)
+            ),
+            pygame.transform.scale(
+                pygame.image.load("assets/waterbig4.PNG").convert(),
                 (WIDTH, HEIGHT)
             )
         ]
@@ -37,11 +47,20 @@ class Game:
 
         while self.running:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+                if event.type == pygame.KEYDOWN:
+
+                    if event.key == pygame.K_e:
+
+                        cat = self.level.get_nearby_cat(self.player)
+
+                        if cat is not None:
+                            self.selected_cat = cat
+
+                    if event.key == pygame.K_ESCAPE:
+                        self.selected_cat = None
 
             self.water_timer += self.clock.get_time()
-            if self.water_timer > 600:
+            if self.water_timer > 800:
                 self.water_timer = 0
                 self.current_water = (self.current_water + 1) % 2
 
@@ -51,15 +70,21 @@ class Game:
             self.player.update(keys)
             self.draw_water()
             self.level.check_boundaries(self.player)
-            if self.level.check_collision(self.player):
+            if self.level.check_collision(self.player.boat):
                 self.player.x = old_x
                 self.player.y = old_y
                 self.player.rect.x = int(self.player.x)
                 self.player.rect.y = int(self.player.y)
             self.draw_water()
 
+
             self.level.draw(self.screen)
             self.player.draw(self.screen)
+            if self.selected_cat is not None:
+                self.ui.draw_cat_info(
+                    self.screen,
+                    self.selected_cat
+                )
             pygame.display.flip()
             self.clock.tick(FPS)
 
