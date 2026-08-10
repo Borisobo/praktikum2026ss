29a0f682c92cf7ad6fb4d5554d09d7791eec756d
Amir-Garifullin7 <garifullinamir.90@gmail.com>
2026-08-10 03:04:17 +0200

    Update game.py

--- game.py
+++ game.py
@@ -15,8 +15,14 @@ class Game:
         self.clock = pygame.time.Clock()
         self.ui = UI(WIDTH, HEIGHT)
         self.selected_cat = None
+        self.cat_menu_open = False
         self.player = Player()
         self.level = Level()
+        self.message = ""
+        self.message_timer = 0
+        self.game_state = "intro"
+        self.title_font = pygame.font.Font(None, 48)
+        self.font = pygame.font.Font(None, 26)
 
         self.water_tiles = [
             pygame.transform.scale(
@@ -36,9 +42,211 @@ class Game:
                 (WIDTH, HEIGHT)
             )
         ]
-
+        self.scroll_image = pygame.image.load("assets/cat_info.PNG").convert()
+        self.scroll_rect = self.scroll_image.get_rect(
+            center=(WIDTH // 2, HEIGHT // 2)
+        )
         self.current_water = 0
         self.water_timer = 0
+
+
+
+    def intro(self):
+        self.screen.blit(
+            self.scroll_image,
+            self.scroll_rect
+        )
+
+        title_font = pygame.font.Font(None, 42)
+        text_font = pygame.font.Font(None, 25)
+        small_font = pygame.font.Font(None, 22)
+
+        center_x = self.scroll_rect.centerx
+
+
+        title = title_font.render(
+            "NYAN CAT: The Arc",
+            True,
+            (70, 45, 25)
+        )
+
+        title_rect = title.get_rect(
+            center=(center_x, self.scroll_rect.top + 280)
+        )
+
+        self.screen.blit(title, title_rect)
+
+        lines = [
+            ""
+            ""
+            "In der Welt der kleinen Katzen",
+            "ist eine schreckliche Überschwemmung",
+            "ausgebrochen.",
+            "",
+            "Das Wasser steigt immer weiter.",
+            "Viele Katzen sitzen auf ihren Inseln fest.",
+            "",
+            "Du bist ein mutiger Katzenretter.",
+            "Mit deinem Floß machst du dich auf den Weg,",
+            "um die Katzen zu retten.",
+        ]
+
+        y = self.scroll_rect.top + 350
+
+        for line in lines:
+
+            if line == "":
+                y += 12
+                continue
+
+            text = text_font.render(
+                line,
+                True,
+                (70, 45, 25)
+            )
+
+            text_rect = text.get_rect(
+                center=(center_x, y)
+            )
+
+            self.screen.blit(text, text_rect)
+
+            y += 20
+
+        warning = text_font.render(
+            "ACHTUNG!",
+            True,
+            (150, 60, 30)
+        )
+
+        warning_rect = warning.get_rect(
+            center=(center_x, y + 20)
+        )
+
+        self.screen.blit(warning, warning_rect)
+
+        warning_text = small_font.render(
+            "Auf deinem Weg gibt es Hindernisse.",
+            True,
+            (70, 45, 25)
+        )
+
+        warning_text_rect = warning_text.get_rect(
+            center=(center_x, y + 50)
+        )
+
+        self.screen.blit(warning_text, warning_text_rect)
+
+        lives_text = small_font.render(
+            "Du hast nur 3 Leben!",
+            True,
+            (70, 45, 25)
+        )
+
+        lives_rect = lives_text.get_rect(
+            center=(center_x, y + 77)
+        )
+
+        self.screen.blit(lives_text, lives_rect)
+
+
+        enter = text_font.render(
+            "ENTER - Weiter",
+            True,
+            (70, 120, 50)
+        )
+
+        enter_rect = enter.get_rect(
+            center=(
+                center_x,
+                self.scroll_rect.bottom - 65
+            )
+        )
+
+        self.screen.blit(enter, enter_rect)
+
+
+    def mission(self):
+
+        self.draw_water()
+        self.level.draw(self.screen)
+        self.player.draw(self.screen)
+        self.screen.blit(
+            self.scroll_image,
+            self.scroll_rect
+        )
+
+        title_font = pygame.font.Font(None, 42)
+        text_font = pygame.font.Font(None, 25)
+        small_font = pygame.font.Font(None, 22)
+
+        center_x = self.scroll_rect.centerx
+        title = title_font.render(
+            "Level 1 Aufgabe",
+            True,
+            (70, 45, 25)
+        )
+
+        title_rect = title.get_rect(
+            center=(center_x, self.scroll_rect.top + 280)
+        )
+
+        self.screen.blit(title, title_rect)
+        lines = [
+            "Rette Katzen von allen Inseln.",
+            "",
+            "Du musst Katzen mit einer",
+            "Bewertung von 4.5 oder höher retten.",
+            "",
+            "Ihre Boat hat erste Level",
+            "und verfügt über nur 2 Plätze",
+            "",
+            "Rette mindestens eine Katze",
+            "von jeder Insel.",
+            "",
+            "ACHTUNG!",
+            "Du hast nur 3 Leben.",
+        ]
+        y = self.scroll_rect.top + 400
+
+        for line in lines:
+
+            if line == "":
+                y += 12
+                continue
+
+            color = (150, 60, 30) if line == "ACHTUNG!" else (70, 45, 25)
+
+            text = text_font.render(
+                line,
+                True,
+                color
+            )
+
+            text_rect = text.get_rect(
+                center=(center_x, y)
+            )
+
+            self.screen.blit(text, text_rect)
+
+            y += 27
+
+        #
+        enter = text_font.render(
+            "ENTER - Spiel starten",
+            True,
+            (70, 120, 50)
+        )
+
+        enter_rect = enter.get_rect(
+            center=(
+                center_x,
+                self.scroll_rect.bottom - 65
+            )
+        )
+
+        self.screen.blit(enter, enter_rect)
+
 
     def draw_water(self):
         self.screen.blit(self.water_tiles[self.current_water], (0, 0))
@@ -47,19 +255,80 @@ class Game:
 
         while self.running:
             for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if self.game_state == "intro":
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_RETURN:
+                            self.game_state = "mission"
+                    continue
+                if self.game_state == "mission":
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_RETURN:
+                            self.game_state = "playing"
+                    continue
                 if event.type == pygame.KEYDOWN:
 
-                    if event.key == pygame.K_e:
-
-                        cat = self.level.get_nearby_cat(self.player)
-
-                        if cat is not None:
-                            self.selected_cat = cat
-
                     if event.key == pygame.K_ESCAPE:
+                        self.cat_menu_open = False
                         self.selected_cat = None
+                        self.cat_message = ""
 
-            self.water_timer += self.clock.get_time()
+                    # E
+                    elif event.key == pygame.K_e:
+
+                        if self.cat_menu_open:
+
+                            if self.selected_cat:
+                                success = self.level.take_cat(
+                                    self.selected_cat,
+                                    self.player.boat
+                                )
+
+                                if success:
+
+                                    self.cat_message = "Katze aufgenommen!"
+                                    self.selected_cat = None
+                                    self.cat_menu_open = False
+
+                                else:
+
+                                    self.cat_message = (
+                                        "Diese Katze passt nicht zur Aufgabe!"
+                                    )
+
+                        else:
+
+                            cat = self.level.get_nearby_cat(
+                                self.player.boat
+                            )
+
+                            if cat:
+
+                                self.selected_cat = cat
+                                self.cat_menu_open = True
+                                self.cat_message = ""
+
+            if self.game_state == "intro":
+                self.draw_water()
+                self.level.draw(self.screen)
+                self.player.draw(self.screen)
+                self.intro()
+
+                pygame.display.flip()
+                self.clock.tick(FPS)
+                continue
+
+            if self.game_state == "mission":
+                self.draw_water()
+                self.level.draw(self.screen)
+                self.player.draw(self.screen)
+                self.mission()
+
+                pygame.display.flip()
+                self.clock.tick(FPS)
+                continue
+
             if self.water_timer > 800:
                 self.water_timer = 0
                 self.current_water = (self.current_water + 1) % 2
@@ -80,12 +349,41 @@ class Game:
 
             self.level.draw(self.screen)
             self.player.draw(self.screen)
-            if self.selected_cat is not None:
+
+            if self.cat_menu_open and self.selected_cat:
                 self.ui.draw_cat_info(
                     self.screen,
-                    self.selected_cat
+                    self.selected_cat,
+                    self.cat_message
                 )
             pygame.display.flip()
+            if self.message_timer > 0:
+                self.message_timer -= 1
+            else:
+                self.message = ""
+
+            if self.message:
+                message_surface = self.ui.font.render(
+                    self.message,
+                    True,
+                    (255, 255, 255)
+                )
+
+                message_rect = message_surface.get_rect(
+                    center=(WIDTH // 2, 100)
+                )
+
+                pygame.draw.rect(
+                    self.screen,
+                    (40, 40, 40),
+                    message_rect.inflate(40, 20)
+                )
+
+                self.screen.blit(
+                    message_surface,
+                    message_rect
+                )
+            self.water_timer += self.clock.get_time()
             self.clock.tick(FPS)
 
 
