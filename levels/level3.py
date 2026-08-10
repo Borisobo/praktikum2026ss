41016e19f85bf14766c6d69d923ec1398c4ba9ad
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-10 20:10:04 +0300

    Update island types and images in level3.py

---
+++ levels/level3.py
@@ -0,0 +1,28 @@
+spawn = (950, 1050)
+
+islands = [
+    {
+        "type": "island5",
+        "image": "assets/islands/island5.png",
+        "x": 300,
+        "y": 250,
+        "width": 300,
+        "height": 250,
+    },
+    {
+        "type": "postoffice",
+        "image": "assets/islands/postoffice.png",
+        "x": 1100,
+        "y": 300,
+        "width": 350,
+        "height": 300,
+    },
+    {
+        "type": "island6",
+        "image": "assets/islands/island6.png",
+        "x": 700,
+        "y": 700,
+        "width": 300,
+        "height": 250,
+    }
+]
