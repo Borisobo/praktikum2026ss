f7e4e6baaf4ee54590cc105969affe262713474b
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-10 17:13:10 +0300

    Update obstacle images for all levels

--- obstacles_data.py
+++ obstacles_data.py
@@ -1,12 +1,12 @@
 OBSTACLES = {
     1: [  # LEVEL 1
         {
-            "image": "assets/obstacles/smth.png",
+            "image": "assets/obstacles/obst1.PNG",
             "x": 800, "y": 500,
             "width": 120, "height": 120
         },
         {
-            "image": "assets/obstacles/smth.png",
+            "image": "assets/obstacles/obst2.PNG",
             "x": 1400, "y": 700,
             "width": 150, "height": 80
         }
@@ -14,12 +14,12 @@ OBSTACLES = {
 
     2: [  # LEVEL 2
         {
-            "image": "assets/obstacles/smth.png",
+            "image": "assets/obstacles/obst3.PNG",
             "x": 600, "y": 400,
             "width": 100, "height": 100
         },
         {
-            "image": "assets/obstacles/smth.png",  
+            "image": "assets/obstacles/obst4.PNG",  
             "x": 900, "y": 300,
             "width": 100, "height": 100
         }
@@ -27,12 +27,12 @@ OBSTACLES = {
 
     3: [ # LEVEL 3
         {
-            "image": "assets/obstacles/smth.png",
+            "image": "assets/obstacles/obst5.PNG",
             "x": 900, "y": 300,
             "width": 200, "height": 200
         },
         {
-            "image": "assets/obstacles/smth.png",  
+            "image": "assets/obstacles/obst1.PNG",  
             "x": 300, "y": 400,
             "width": 120, "height": 120
       }
@@ -40,12 +40,12 @@ OBSTACLES = {
 
     4: [ # LEVEL 4
         {
-            "image": "assets/obstacles/smth.png",
+            "image": "assets/obstacles/obst2.PNG",
             "x": 1200, "y": 900,
             "width": 130, "height": 130
         },
         {
-            "image": "assets/obstacles/smth.png",
+            "image": "assets/obstacles/obst3.PNG",
             "x": 500, "y": 600,
             "width": 130, "height": 130
         }
@@ -53,12 +53,12 @@ OBSTACLES = {
 
     5: [ # LEVEL 5
         {
-            "image": "assets/obstacles/smth.png",
+            "image": "assets/obstacles/obst4.PNG",
             "x": 400, "y": 600,
             "width": 160, "height": 160
         },
         {
-            "image": "assets/obstacles/smth.png",
+            "image": "assets/obstacles/obst5.PNG",
             "x": 1500, "y": 500,
             "width": 160, "height": 160
         }
