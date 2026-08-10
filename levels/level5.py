c88b902d117c444e244679d62d73724a3eaf8d9e
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-10 20:19:49 +0300

    Update island types and images in level5.py

---
+++ levels/level5.py
@@ -0,0 +1,36 @@
+spawn = (950, 1050)
+
+islands = [
+    {
+        "type": "island8",
+        "image": "assets/islands/island8.png",
+        "x": 300,
+        "y": 150,
+        "width": 300,
+        "height": 250,
+    },
+    {
+        "type": "greenhouse",
+        "image": "assets/islands/greenhouse.png",
+        "x": 1100,
+        "y": 300,
+        "width": 350,
+        "height": 300,
+    },
+    {
+        "type": "island9",
+        "image": "assets/islands/island9.png",
+        "x": 700,
+        "y": 700,
+        "width": 300,
+        "height": 250,
+    },
+    {
+        "type": "townhall",
+        "image": "assets/islands/townhall.png",
+        "x": 450,
+        "y": 400 ,
+        "width": 300,
+        "height": 250,
+    }
+]
