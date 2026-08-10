bddbf548da59489aac86481c6d4f6dc971546367
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-10 16:34:37 +0300

    Add obstacle data for multiple levels

--- obstacles_data.py
+++ obstacles_data.py
@@ -1,1 +1,68 @@
+OBSTACLES = {
+    1: [  # LEVEL 1
+        {
+            "image": "assets/obstacles/smth.png",
+            "x": 800, "y": 500,
+            "width": 120, "height": 120
+        },
+        {
+            "image": "assets/obstacles/smth.png",
+            "x": 1400, "y": 700,
+            "width": 150, "height": 80
+        }
+    ],
+
+    2: [  # LEVEL 2
+        {
+            "image": "assets/obstacles/smth.png",
+            "x": 600, "y": 400,
+            "width": 100, "height": 100
+        },
+        {
+            "image": "assets/obstacles/smth.png",  
+            "x": 900, "y": 300,
+            "width": 100, "height": 100
+        }
+    ],
+
+    3: [ # LEVEL 3
+        {
+            "image": "assets/obstacles/smth.png",
+            "x": 900, "y": 300,
+            "width": 200, "height": 200
+        },
+        {
+            "image": "assets/obstacles/smth.png",  
+            "x": 300, "y": 400,
+            "width": 120, "height": 120
+      }
+    ],
+
+    4: [ # LEVEL 4
+        {
+            "image": "assets/obstacles/smth.png",
+            "x": 1200, "y": 900,
+            "width": 130, "height": 130
+        },
+        {
+            "image": "assets/obstacles/smth.png",
+            "x": 500, "y": 600,
+            "width": 130, "height": 130
+        }
+    ],
+
+    5: [ # LEVEL 5
+        {
+            "image": "assets/obstacles/smth.png",
+            "x": 400, "y": 600,
+            "width": 160, "height": 160
+        },
+        {
+            "image": "assets/obstacles/smth.png",
+            "x": 1500, "y": 500,
+            "width": 160, "height": 160
+        }
+    ]
+}
+
 
