ddd8a7b7144b15000787f960612c766d41e55061
Borisobo <113468649+Borisobo@users.noreply.github.com>
2026-08-13 22:44:27 +0200

    Change character locations in cats_data_nps.py
    Updated locations for various characters in the dataset.

--- cats_data_nps.py
+++ cats_data_nps.py
@@ -50,21 +50,21 @@ CATS = {
                 "beruf": "Zimmermann",
                 "qualities": ["stark", "zuverlässig", "zielstrebig"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (450, 785)
             },
             {
                 "name": "Coco",
                 "beruf": "Bäckerin",
                 "qualities": ["großzügig", "kreativ", "freundlich"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (335, 875)
             },
             {
                 "name": "Leo",
                 "beruf": "Elektriker",
                 "qualities": ["erfinderisch", "konzentriert", "intelligent"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (565, 875)
             }
         ],
 
@@ -74,21 +74,21 @@ CATS = {
                 "beruf": "Verkäufer",
                 "qualities": ["kommunikativ", "höflich", "geduldig"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (1375, 400)
             },
             {
                 "name": "Bella",
                 "beruf": "Lagerarbeiterin",
                 "qualities": ["organisiert", "ordentlich", "verantwortungsbewusst"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (1235, 400)
             },
             {
                 "name": "Charly",
                 "beruf": "Kassierer",
                 "qualities": ["ehrlich", "genau", "pünktlich"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (1515, 400)
             }
         ],
 
@@ -98,21 +98,21 @@ CATS = {
                 "beruf": "Fotografin",
                 "qualities": ["kreativ", "aufmerksam", "neugierig"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (1400, 685)
             },
             {
                 "name": "Rocky",
                 "beruf": "Unruhestifter",
                 "qualities": ["humorvoll", "stur", "unhöflich"],
                 "rating": 3.5,
-                "location": (0, 0)
+                "location": (1285, 775)
             },
             {
                 "name": "Daisy",
                 "beruf": "Betrüger",
                 "qualities": ["egoistisch", "unzuverlässig", "stur"],
                 "rating": 3.0,
-                "location": (0, 0)
+                "location": (1535, 775)
             }
         ]
     },
@@ -125,21 +125,21 @@ CATS = {
                 "beruf": "Kapitän",
                 "qualities": ["mutig", "zuverlässig", "entschlossen"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (450, 770)
             },
             {
                 "name": "Emma",
                 "beruf": "Navigatorin",
                 "qualities": ["klug", "aufmerksam", "vorsichtig"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (320, 875)
             },
             {
                 "name": "Finn",
                 "beruf": "Ingenieur",
                 "qualities": ["erfinderisch", "intelligent", "konzentriert"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (580, 875)
             }
         ],
 
@@ -149,21 +149,21 @@ CATS = {
                 "beruf": "Postbotin",
                 "qualities": ["pünktlich", "freundlich", "zuverlässig"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (1375, 270)
             },
             {
                 "name": "Sammy",
                 "beruf": "Briefsortierer",
                 "qualities": ["ordentlich", "genau", "geduldig"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (1220, 400)
             },
             {
                 "name": "Kira",
                 "beruf": "Kurierin",
                 "qualities": ["schnell", "mutig", "hilfsbereit"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (1530, 400)
             }
         ],
 
@@ -173,28 +173,28 @@ CATS = {
                 "beruf": "Leuchtturmwärter",
                 "qualities": ["weise", "aufmerksam", "verantwortungsbewusst"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (1250, 670)
             },
             {
                 "name": "Ruby",
                 "beruf": "Taschendieb",
                 "qualities": ["unmotiviert", "leichtsinnig", "hinterlistig"],
                 "rating": 3.0,
-                "location": (0, 0)
+                "location": (1120, 775)
             },
             {
                 "name": "Whisper",
                 "beruf": "Schatzsucher",
                 "qualities": ["geheimnisvoll", "neugierig", "vorsichtig"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (1380, 775)
             },
             {
                 "name": "Oscar",
                 "beruf": "Handwerker",
                 "qualities": ["unfreundlich", "misstrauisch", "pessimistisch"],
                 "rating": 3.5,
-                "location": (0, 0)
+                "location": (1340, 770)
             }
         ]
     },
@@ -207,21 +207,21 @@ CATS = {
                 "beruf": "Barista",
                 "qualities": ["freundlich", "kommunikativ", "fröhlich"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (450, 770)
             },
             {
                 "name": "Gustav",
                 "beruf": "Konditor",
                 "qualities": ["kreativ", "ordentlich", "geduldig"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (320, 875)
             },
             {
                 "name": "Mimi",
                 "beruf": "Kellnerin",
                 "qualities": ["höflich", "schnell", "aufmerksam"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (580, 875)
             }
         ],
 
@@ -231,14 +231,14 @@ CATS = {
                 "beruf": "Fotograf",
                 "qualities": ["neugierig", "kreativ", "aufmerksam"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (1375, 270)
             },
             {
                 "name": "Hazel",
                 "beruf": "Künstlerin",
                 "qualities": ["nachlässig", "offen", "ängstlich"],
                 "rating": 3.5,
-                "location": (0, 0)
+                "location": (1220, 400)
             }
         ],
 
@@ -248,21 +248,21 @@ CATS = {
                 "beruf": "Trainer",
                 "qualities": ["diszipliniert", "zielstrebig", "sportlich"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (1250, 670)
             },
             {
                 "name": "Sunny",
                 "beruf": "Schwimmerin",
                 "qualities": ["ausdauernd", "mutig", "sportlich"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (1120, 775)
             },
             {
                 "name": "Hugo",
                 "beruf": "Rettungsschwimmer",
                 "qualities": ["stark", "zuverlässig", "fürsorglich"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (1380, 775)
             }
         ]
     },
@@ -275,14 +275,14 @@ CATS = {
                 "beruf": "Forscher",
                 "qualities": ["wissbegierig", "intelligent", "ausdauernd"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (450, 325)
             },
             {
                 "name": "Rosie",
                 "beruf": "Tierpflegerin",
                 "qualities": ["fürsorglich", "geduldig", "freundlich"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (380, 325)
             }
         ],
 
@@ -292,21 +292,21 @@ CATS = {
                 "beruf": "Gärtnerin",
                 "qualities": ["naturverbunden", "fleißig", "geduldig"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (1375, 350)
             },
             {
                 "name": "Ivy",
                 "beruf": "Botanikerin",
                 "qualities": ["klug", "vorsichtig", "neugierig"],
                 "rating": 4.5,
-                "location": (0, 0)
+                "location": (1280, 350)
             },
             {
                 "name": "Basil",
                 "beruf": "Biologe",
                 "qualities": ["intelligent", "ruhig", "verantwortungsbewusst"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (1470, 350)
             }
         ],
 
@@ -316,21 +316,21 @@ CATS = {
                 "beruf": "Jäger",
                 "qualities": ["geizig", "mutig", "unordentlich"],
                 "rating": 3.5,
-                "location": (0, 0)
+                "location": (450, 875)
             },
             {
                 "name": "Peanut",
                 "beruf": "Schneider",
                 "qualities": ["geschickt", "ordentlich", "kreativ"],
                 "rating": 4.0,
-                "location": (0, 0)
+                "location": (380, 875)
             },
             {
                 "name": "Snow",
                 "beruf": "Arzt",
                 "qualities": ["hilfsbereit", "weise", "zuverlässig"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (520, 875)
             }
         ],
 
@@ -340,7 +340,7 @@ CATS = {
                 "beruf": "Bürgermeister",
                 "qualities": ["gerecht", "loyal", "diplomatisch"],
                 "rating": 5.0,
-                "location": (0, 0)
+                "location": (1350, 875)
             }
         ]
     }
